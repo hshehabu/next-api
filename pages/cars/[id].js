08a4a33ccae0800249b305f2c7c2479609b147fc
hshehabu <hamzashehabu457@gmail.com>
2024-10-11 09:28:10 +0300

    implemeted ssg

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -1,14 +1,45 @@
 import { useRouter } from "next/router";
 
-export default function Car(){
-    const router = useRouter()
+import Head from "next/head";
 
-    const {id} = router.query
+export default function Car({ car }) {
+  const router = useRouter();
 
-    return (
-        <div>
-            <h1>Car Details: {id}</h1>
-        </div>
-    )
- 
+  const { id } = router.query;
+
+  return (
+    <div>
+      <Head>
+        <title>
+          {car.color} {car.id}
+        </title>
+      </Head>
+      <h1>Car Details: {id}</h1>
+      <img src={car.image} />
+    </div>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const req = await fetch(`http://localhost:3000/${params.id}.json`);
+
+  const res = await req.json();
+
+  return {
+    props: { car: res },
+  };
+}
+
+export async function getStaticPaths() {
+  const req = await fetch("http://localhost:3000/cars.json");
+  const data = await req.json();
+
+  const paths = data.map((car) => {
+    return { params: { id: car } };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
